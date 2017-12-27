bce25efb1d2f7b6b75b6aa0c643f1e7ebcfb2ab3
iceqiw <qqwei1123@163.com>
2017-12-27 17:59:05 +0800

    aaa

--- train/common.py
+++ train/common.py
@@ -18,5 +18,5 @@ hd = {
     'accept-language':
     "zh-CN,zh;q=0.8,en;q=0.6",
     'cookie':
-    "JSESSIONID=5EC5B12A73DC6D000484A99FCCD747FD; _jc_save_detail=true; RAIL_OkLJUJ=FFB2V0iAeAjwLyc10PPqi6w7T9pGMmAO; route=495c805987d0f5c8c84b14f60212447d; BIGipServerotn=2045247754.24610.0000; BIGipServerpassport=937951498.50215.0000; RAIL_EXPIRATION=1510445794929; RAIL_DEVICEID=akRjCfxqcpM2fMOAqm4CJZzidINzPVjLTZUAJrs1Ll9h6UEosMCFKmdzs7P6rs2kQKbV9hgdjes1GXzlmYyMY8HRHe6tLjTviB9b6U6tO6vLwQelergVyuvD-barRmYQYB1_7iVm0FJ10cGgspHJGhFSygFjD-Zf; _jc_save_fromStation=%u798F%u5DDE%2CFZS; _jc_save_toStation=%u897F%u5B89%2CXAY; _jc_save_fromDate=2017-11-10; _jc_save_toDate=2017-11-08; _jc_save_wfdc_flag=dc"
+    "JSESSIONID=5CABAA927C870655B853A255CFFDB1E3; tk=pNogcAkbGN8AtUUqNaBv8vcFVlrefEfLwQSoSwijq1q0; RAIL_EXPIRATION=1514530584881; RAIL_DEVICEID=q5ztWh4ue0k2CNXwc3i060RfZf40DEPxMOgE4GRB2uDbGNE5xWF8XpBfpenAhUKLi3hykaggFuH9j7a0sLRub_6nWDABplZ0f3vWbwDPM8XfRHtvuY2KrXsTh-hFCsgAMiT0wO9eashzihlcjhWU6KrG9SpVTE2C; route=495c805987d0f5c8c84b14f60212447d; BIGipServerotn=1977155850.50210.0000; BIGipServerpassport=786956554.50215.0000; current_captcha_type=Z; _jc_save_fromStation=%u798F%u5DDE%2CFZS; _jc_save_toStation=%u4E4C%u9C81%u6728%u9F50%2CWAR; _jc_save_fromDate=2018-01-25; _jc_save_toDate=2017-12-27; _jc_save_wfdc_flag=dc"
 }
